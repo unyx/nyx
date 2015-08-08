9cbf7d8fcc9ab1d53da90aea9c8c28d0540b4bb6
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-08 08:43:36 +0200

    [Core] Stringable: Implicit __toString() must not throw exceptions, while the explicit toString() may throw them.

--- src/core/interfaces/Stringable.php
+++ src/core/interfaces/Stringable.php
@@ -10,7 +10,8 @@
  * but not necessarily the format of the string.
  *
  * The preferred method to use is the explicit {@see self::toString()}. Implementations should alias the implicit,
- * magic method to its explicit counterpart, so that overriding only requires the modification of one method.
+ * magic method to its explicit counterpart, so that overriding only requires the modification of one method, but
+ * should wrap the call in a try/catch block to ensure conformity with PHP's standard handling of __toString().
  *
  * @package     Nyx\Core
  * @version     0.1.0
@@ -23,12 +24,16 @@ interface Stringable
     /**
      * Returns the string representation of the object.
      *
+     * Note: This method MAY throw exceptions.
+     *
      * @return  string
      */
     public function toString() : string;
 
     /**
      * {@see self::toString()}
+     *
+     *  Note: This method MUST NOT throw exceptions, opposite to the explicit self::toString() method.
      */
     public function __toString() : string;
 }
