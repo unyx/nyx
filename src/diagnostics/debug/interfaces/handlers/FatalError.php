0f9d0f02f31f3bec751a8ed80631fe8f05187ef9
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-08 13:54:09 +0200

    [Diagnostics/Debug] Added a Fatal Error Handler interface.

---
+++ src/diagnostics/debug/interfaces/handlers/FatalError.php
@@ -0,0 +1,21 @@
+<?php namespace nyx\diagnostics\debug\interfaces\handlers;
+
+/**
+ * Fatal Error Handler Interface
+ *
+ * @package     Nyx\Diagnostics\Debug
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2016 Nyx Dev Team
+ * @link        http://docs.muyo.io/nyx/diagnostics/debug.html
+ * @todo        Move into core as OnShutdown interface?
+ */
+interface FatalError
+{
+    /**
+     * Handles error-related cleanups. Intended to be registered as shutdown function with PHP. Implementations
+     * should attempt to check if any error occurred before shutdown and if so, determine whether it was a fatal
+     * error and handle it gracefully.
+     */
+    public function onShutdown();
+}
