dc311522fc9f663443dec289426418c769eb9043
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-08 14:17:35 +0200

    [Core/Collections] Added a Set interface.

---
+++ src/core/collections/interfaces/Set.php
@@ -0,0 +1,21 @@
+<?php namespace nyx\core\collections\interfaces;
+
+/**
+ * Set Interface
+ *
+ * @package     Nyx\Core\Collections
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2016 Nyx Dev Team
+ * @link        http://docs.muyo.io/nyx/core/collections.html
+ */
+interface Set extends Collection
+{
+    /**
+     * Sets the given item in the Collection.
+     *
+     * @param   mixed   $item   The item to set.
+     * @return  $this
+     */
+    public function set($item);
+}
