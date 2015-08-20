29981620229bcd21cc12fff00360d2934ad74491
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-20 21:46:42 +0200

    [Diagnostics/Debug] Dumpers: Added a bridge for ChromeLogger.

---
+++ src/diagnostics/debug/dumpers/ChromeLogger.php
@@ -0,0 +1,31 @@
+<?php namespace nyx\diagnostics\debug\dumpers;
+
+// Internal dependencies
+use nyx\diagnostics\debug;
+
+/**
+ * Chrome Logger Dumper
+ *
+ * A bridge allowing to use Chrome Logger as a Dumper within the Debug subcomponent. Check out Chrome Logger itself
+ * on {@see http://craig.is/writing/chrome-logger} and its PHP variant on Github
+ * {@see https://github.com/ccampbell/chromephp}.
+ *
+ * Requires:
+ * - Package: ccampbell/chromephp (available as suggestion for nyx/diagnostics within Composer)
+ *
+ * @package     Nyx\Diagnostics\Debug
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2016 Nyx Dev Team
+ * @link        http://docs.muyo.io/nyx/diagnostics/debug.html
+ */
+class ChromeLogger implements debug\interfaces\Dumper
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function dump(...$vars)
+    {
+        call_user_func_array('ChromePhp::log', $vars);
+    }
+}
