b0b83049bab8b42e29dbd2cd9ff75d40962b304e
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-20 22:11:08 +0200

    [Diagnostics/Debug] Dumpers: Added a var_dump() wrapper Dumper.

---
+++ src/diagnostics/debug/dumpers/Native.php
@@ -0,0 +1,26 @@
+<?php namespace nyx\diagnostics\debug\dumpers;
+
+// Internal dependencies
+use nyx\diagnostics\debug\interfaces;
+
+/**
+ * Native Dumper
+ *
+ * Uses var_dump() to perform the dump.
+ *
+ * @package     Nyx\Diagnostics\Debug
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2016 Nyx Dev Team
+ * @link        http://docs.muyo.io/nyx/diagnostics/debug.html
+ */
+class Native implements interfaces\Dumper
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function dump(...$vars)
+    {
+        call_user_func_array('var_dump', $vars);
+    }
+}
