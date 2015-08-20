275270f94cbd3eac59b0fc6071b9147c63a9fe39
Michał Chojnacki <m.chojnacki@muyo.io>
2015-08-20 22:23:08 +0200

    [Diagnostics/Debug] Dumpers: Added a bridge for LadyBug.

---
+++ src/diagnostics/debug/dumpers/Ladybug.php
@@ -0,0 +1,61 @@
+<?php namespace nyx\diagnostics\debug\dumpers;
+
+// Vendor dependencies
+use Ladybug\Dumper;
+
+// Internal dependencies
+use nyx\diagnostics\debug\interfaces;
+
+/**
+ * Ladybug Dumper
+ *
+ * A bridge allowing to use Ladybug as a Dumper within the Debug subcomponent. The class also gives you access
+ * to the underlying Ladybug\Dumper instance if you want to customize its settings. Check out Ladybug itself on
+ * Github at {@see https://github.com/raulfraile/Ladybug}.
+ *
+ * Requires:
+ * - Package: raulfraile/ladybug (available as suggestion for nyx/diagnostics within Composer)
+ *
+ * @package     Nyx\Diagnostics\Debug
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2016 Nyx Dev Team
+ * @link        http://docs.muyo.io/nyx/diagnostics/debug.html
+ */
+class Ladybug implements interfaces\Dumper
+{
+    /**
+     * @var Dumper  The underlying instance of the Ladybug\Dumper.
+     */
+    private $dumper;
+
+    /**
+     * Constructs a new Dumper bridge for an Ladybug\Dumper instance.
+     *
+     * @param   Dumper  $dumper     An already instantiated Ladybug\Dumper instance or null to construct a new one.
+     */
+    public function __construct(Dumper $dumper = null)
+    {
+        $this->dumper = $dumper ?: new Dumper;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function dump(...$vars)
+    {
+        // @todo Actual output handling.
+        echo call_user_func_array([$this->dumper, "dump"], $vars);
+    }
+
+    /**
+     * Returns the underlying Ladybug\Dumper instance.
+     *
+     * @return  Dumper
+     * @todo    Rename to getLadybug or...?
+     */
+    public function expose()
+    {
+        return $this->dumper;
+    }
+}
