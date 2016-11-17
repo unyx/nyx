b15653cd410aca03f6b1ed47f181bbd4ccf038ac
Michał Chojnacki <m.chojnacki@muyo.io>
2016-11-18 00:01:52 +0100

    [Notify] Added a Mail Driver interface.

---
+++ src/notify/transports/mail/interfaces/Driver.php
@@ -0,0 +1,22 @@
+<?php namespace nyx\notify\transports\mail\interfaces;
+
+/**
+ * Mail Driver Interface
+ *
+ * @package     Nyx\Notify
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
+ */
+interface Driver
+{
+    /**
+     * Sends a Mail Message.
+     *
+     * @param   \Swift_Mime_Message $message            The Message to send.
+     * @param   array               $failedRecipients   A reference to an array which will hold data about all requested
+     *                                                  recipients sending to whom failed.
+     */
+    public function send(\Swift_Mime_Message $message, array &$failedRecipients = null);
+}
