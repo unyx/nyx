c70b5b3631023a85a1f0755fb12b0e3b38b9e750
Michał Chojnacki <m.chojnacki@muyo.io>
2016-11-22 10:46:51 +0100

    [Notify] Added a Notify Service Facade.

---
+++ src/notify/Facade.php
@@ -0,0 +1,22 @@
+<?php namespace nyx\notify;
+
+/**
+ * Notify Service Facade
+ *
+ * @package     Nyx\Notify
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
+ * @see         TransportManager
+ */
+class Facade extends \Illuminate\Support\Facades\Facade
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected static function getFacadeAccessor()
+    {
+        return TransportManager::class;
+    }
+}
