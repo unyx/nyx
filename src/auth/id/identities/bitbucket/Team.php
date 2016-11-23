d6ba2aeeb99903c32dced7fd75a66d32e018d000
Michał Chojnacki <m.chojnacki@muyo.io>
2016-11-23 12:29:07 +0100

    [Auth/ID] Added a Bitbucket Team Identity subtype (OAuth 2.0).

---
+++ src/auth/id/identities/bitbucket/Team.php
@@ -0,0 +1,18 @@
+<?php namespace nyx\auth\id\identities\bitbucket;
+
+// Internal dependencies
+use nyx\auth\id\identities;
+
+/**
+ * Bitbucket Team Identity
+ *
+ * @package     Nyx\Auth
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
+ */
+class Team extends identities\Bitbucket
+{
+
+}
