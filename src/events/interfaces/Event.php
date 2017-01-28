4ffa49573db2ce23d6beb08d612c3aacddf5217b
Michał Chojnacki <m.chojnacki@muyo.io>
2017-01-29 00:48:37 +0100

    [Events] Simplified interfaces\Event: - no longer implements core\interfaces\Named (now has a getType() method instead); - removed event propagation stopping (may return along a worthwhile use-case); - removed emitter awareness (events may go through several emitters and the meaning of "emitter" or "origin" highly depends on the context of the use-case, and thus should not be presumed);

--- src/events/interfaces/Event.php
+++ src/events/interfaces/Event.php
@@ -1,30 +1,21 @@
 <?php namespace nyx\events\interfaces;
 
-// External dependencies
-use nyx\core;
-
 /**
  * Event Interface
  *
  * @package     Nyx\Events
  * @version     0.1.0
  * @author      Michal Chojnacki <m.chojnacki@muyo.io>
- * @copyright   2012-2016 Nyx Dev Team
- * @link        http://docs.muyo.io/nyx/events/index.html
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
+ * @todo        Decide: Reintroduce propagation halting?
  */
-interface Event extends core\interfaces\Named, EmitterAware
+interface Event
 {
     /**
-     * Checks whether no further Event listeners should be invoked for this Event.
+     * Returns the type of this Event.
      *
-     * @return  bool    True if no further Event listeners should be invoked for this Event, false otherwise.
+     * @return  string
      */
-    public function stopped() : bool;
-
-    /**
-     * Stops the propagation of the Event to further listeners.
-     *
-     * @return  $this
-     */
-    public function stop() : self;
+    public function getType() : string;
 }
