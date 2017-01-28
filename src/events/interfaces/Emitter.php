97ac0d4692c096b103799b9a963c90a9aee43a8f
Michał Chojnacki <m.chojnacki@muyo.io>
2017-01-28 23:47:43 +0100

    [Events] Simplified interfaces\Emitter::emit() to get rid of some complexity in how it can be invoked and to allow for very basic emission scenarios with less overhead. Fixed method signatures while at it.

--- src/events/interfaces/Emitter.php
+++ src/events/interfaces/Emitter.php
@@ -3,68 +3,46 @@
 /**
  * Event Emitter Interface
  *
- * Event synchronization point. Registers/removes listeners for events and triggers events. Supports registering
- * listeners by priority.
- *
  * @package     Nyx\Events
  * @version     0.1.0
  * @author      Michal Chojnacki <m.chojnacki@muyo.io>
- * @copyright   2012-2016 Nyx Dev Team
- * @link        http://docs.muyo.io/nyx/events/index.html
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
  */
 interface Emitter
 {
     /**
-     * Magic constant used by {@see self::emit()}. See the docblock for the $event argument therein.
-     */
-    const CREATE_EMPTY_EVENT = '__emitter_create_empty_event';
-
-    /**
      * Triggers the listeners of a given event.
      *
-     * You may pass any additional arguments you wish to this method. They will be passed along to the listeners,
-     * with the propagated Event instance always being the first argument (the $name will not be passed directly
-     * as it will be available through the Event). However, be aware of what is passed as reference and what is not,
-     * if you wish to modify the data within listeners and have it available with those changes within further
-     * listeners.
-     *
-     * @param   string|Event    $name           The name of the event to emit. You may also pass an Event object that
-     *                                          has its trigger name set {@see Event::setName()}.
-     * @param   Event|mixed     $event          The event to pass to the event handlers/listeners. If not supplied or
-     *                                          not an instance of events\Event an empty Event instance will be
-     *                                          created and all arguments after $name will be passed to the
-     *                                          listeners. If left as the CREATE_EMPTY_EVENT magic constant, this
-     *                                          argument will be completely ignored (ie. not passed to the listeners)
-     *                                          but will still trigger the creation of a base Event.
-     * @param   mixed           ...$arguments   The data to pass to listeners along with the Event, in the order given.
-     * @return  Event                           The Event after it has run through all registered listeners.
-     * @throws  \LogicException                 When only an Event object is passed but it does not have a valid
-     *                                          trigger name.
+     * @param   string|Event    $event          The name of the event to emit or an object implementing the Event
+     *                                          interface, whose name will be used.
+     * @param   mixed           ...$payload     The data to pass to listeners, in the order given. If $event is an
+     *                                          instance of the Event interface, it will be prepended to the $payload.
+     * @throws  \InvalidArgumentException       When $event is neither a string nor an Event instance.
      */
-    public function emit($name, $event = self::CREATE_EMPTY_EVENT, ...$arguments) : Event;
+    public function emit($event, ...$payload);
 
     /**
      * Adds an event listener that listens to the specified event.
      *
-     * @param   string      $name           The event to listen for.
-     * @param   callable    $listener       The listener to add.
-     * @param   integer     $priority       The higher this value, the earlier an event listener will be triggered
-     *                                      in the chain.
+     * @param   string   $event             The event to listen for.
+     * @param   callable $listener          The listener to add.
+     * @param   integer  $priority          The higher this value, the earlier an event listener will be triggered.
      * @return  $this
      */
-    public function on(string $name, callable $listener, int $priority = 0) : self;
+    public function on(string $event, callable $listener, int $priority = 0) : Emitter;
 
     /**
      * Adds an event listener that listens to the specified event but only gets fired once and then removed from
      * the stack.
      *
-     * @param   string      $name           The event to listen for.
+     * @param   string      $event          The event to listen for.
      * @param   callable    $listener       The listener to add.
      * @param   integer     $priority       The higher this value, the earlier an event listener will be triggered
      *                                      in the chain.
      * @return  $this
      */
-    public function once(string $name, callable $listener, int $priority = 0) : self;
+    public function once(string $event, callable $listener, int $priority = 0) : Emitter;
 
     /**
      * Removes the specified listener from the stack of a given event, OR
@@ -72,11 +50,11 @@ interface Emitter
      * removes the given listener from all events it's listening to when only $name is null, OR
      * removes all listeners registered in this Emitter if both $name and $listener are null,
      *
-     * @param   string    $name       The event to remove a listener from.
-     * @param   callable  $listener   The listener to remove.
+     * @param   string      $event      The event to remove a listener from.
+     * @param   callable    $listener   The listener to remove.
      * @return  $this
      */
-    public function off(string $name = null, callable $listener = null) : self;
+    public function off(string $event = null, callable $listener = null) : Emitter;
 
     /**
      * Registers an Event Subscriber with this Emitter. The Subscriber is queried for a list of events
@@ -85,7 +63,7 @@ interface Emitter
      * @param   Subscriber  $subscriber
      * @return  $this
      */
-    public function register(Subscriber $subscriber) : self;
+    public function register(Subscriber $subscriber) : Emitter;
 
     /**
      * Deregisters an Event Subscriber from this Emitter, removing all listeners it has registered
@@ -94,7 +72,7 @@ interface Emitter
      * @param   Subscriber  $subscriber
      * @return  $this
      */
-    public function deregister(Subscriber $subscriber) : self;
+    public function deregister(Subscriber $subscriber) : Emitter;
 
     /**
      * Returns an array containing the priority-sorted listeners for the given event or for all events if no trigger
