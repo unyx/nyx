dbee548cff718c3e4daefd294a8295a4deb53088
Michał Chojnacki <m.chojnacki@muyo.io>
2017-01-28 19:59:41 +0100

    [Core] interfaces\Named: Method signatures and docblocks.

--- src/core/interfaces/Named.php
+++ src/core/interfaces/Named.php
@@ -3,29 +3,31 @@
 /**
  * Named Interface
  *
- * A Named object is one that has a name which can be get and set. Implementations MUST ensure that the a non-empty
- * name is set no later than before the first use of the inheriting object, ideally during construction.
+ * A Named object is one that has a name which can be get and set.
+ *
+ * Implementations MUST ensure that the a non-empty name is set no later than before the first use of
+ * the implementing object, ideally during construction.
  *
  * @package     Nyx\Core
  * @version     0.1.0
  * @author      Michal Chojnacki <m.chojnacki@muyo.io>
- * @copyright   2012-2016 Nyx Dev Team
- * @link        http://docs.muyo.io/nyx/core/index.html
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
  */
 interface Named
 {
     /**
-     * Returns the name of the implementer of this interface.
+     * Returns the name of this object.
      *
      * @return  string
      */
     public function getName() : string;
 
     /**
-     * Sets the name of the implementer of this interface.
+     * Sets the name of this object.
      *
      * @param   string  $name   The name to set.
      * @return  $this
      */
-    public function setName(string $name);
+    public function setName(string $name) : Named;
 }
