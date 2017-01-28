6a2c3c724050096e74b0f52c8da3c733e2a28a2e
Michał Chojnacki <m.chojnacki@muyo.io>
2017-01-28 20:33:17 +0100

    [Events] interfaces\EmitterAware: - allowed getEmitter() to return null if no Emitter is set; - fixed the signature of setEmitter(); - docblocks and CS;

--- src/events/interfaces/EmitterAware.php
+++ src/events/interfaces/EmitterAware.php
@@ -3,34 +3,34 @@
 /**
  * Event Emitter Aware Interface
  *
- * An Event Emitter Aware object is one that may contain an Emitter which can be injected and retrieved using the
- * respective getters/setters.
+ * An Event Emitter Aware object is one that may have access to an Emitter, which can be injected and
+ * retrieved using the respective getters and setters.
  *
  * @package     Nyx\Events
  * @version     0.1.0
  * @author      Michal Chojnacki <m.chojnacki@muyo.io>
- * @copyright   2012-2016 Nyx Dev Team
- * @link        http://docs.muyo.io/nyx/events/index.html
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
  */
 interface EmitterAware
 {
     /**
-     * Returns the Event Emitter instance in use by the implementer.
+     * Returns the Event Emitter instance this object is using, if any.
      *
      * @return  Emitter
      */
-    public function getEmitter() : Emitter;
+    public function getEmitter() : ?Emitter;
 
     /**
-     * Sets an Event Emitter instance inside the implementer.
+     * Sets the Event Emitter instance this object should have access to.
      *
      * @param   Emitter  $emitter   The Emitter to set.
      * @return  $this
      */
-    public function setEmitter(Emitter $emitter) : self;
+    public function setEmitter(Emitter $emitter) : EmitterAware;
 
     /**
-     * Checks whether the implementer has a set Event Emitter instance.
+     * Checks whether the object has access to an Event Emitter instance.
      *
      * @return  bool    True when an Event Emitter is set, false otherwise.
      */
