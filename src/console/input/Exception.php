bbe5119bacf446c971564420b51ff8feb1001dc8
Michał Chojnacki <m.chojnacki@muyo.io>
2017-02-05 17:19:40 +0100

    [Console/Input] Added base Exception class for runtime errors resulting from invalid input.

---
+++ src/console/input/Exception.php
@@ -0,0 +1,14 @@
+<?php namespace nyx\console\input;
+
+/**
+ * Input Exception
+ *
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
+ */
+class Exception extends \RuntimeException
+{
+
+}
