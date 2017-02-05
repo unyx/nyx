30153872ede4075e8f2bc6cb32aa436ab44f5df8
Michał Chojnacki <m.chojnacki@muyo.io>
2017-02-05 17:21:00 +0100

    [Console/Input] Added input\exceptions\InvalidParameters.

---
+++ src/console/input/exceptions/InvalidParameters.php
@@ -0,0 +1,17 @@
+<?php namespace nyx\console\input\exceptions;
+
+// Internal dependencies
+use nyx\console\input;
+
+/**
+ * Invalid Parameters Exception
+ *
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
+ */
+class InvalidParameters extends input\Exception
+{
+
+}
