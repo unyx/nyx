49fa7c43ad7312ba0e1e3e52c9a834755aed8792
Michał Chojnacki <m.chojnacki@muyo.io>
2017-02-06 12:47:06 +0100

    [Console/Input] Let interfaces\input\Tokens extend core\collections\interfaces\Map instead of the base Collection interface. Also renamed arguments()/options() to getArguments()/getOptions().

--- src/console/interfaces/input/Tokens.php
+++ src/console/interfaces/input/Tokens.php
@@ -10,29 +10,24 @@ use nyx\core\collections;
  * Implementations of this interface must be able to distinguish Input Options from Arguments in the collection,
  * provided they are given a collection of tokens in a format recognized by them.
  *
- * This interface does not dictate the type of Collection that needs to be implemented nor does it require
- * the tokens to be in immutable sequence order.
- *
- * @package     Nyx\Console
  * @version     0.1.0
  * @author      Michal Chojnacki <m.chojnacki@muyo.io>
  * @copyright   2012-2017 Nyx Dev Team
  * @link        https://github.com/unyx/nyx
  */
-interface Tokens extends collections\interfaces\Collection
+interface Tokens extends collections\interfaces\Map
 {
     /**
-     * Returns all tokens which do not start with a hyphen and therefore *appear to* be arguments.
+     * Returns all tokens which *appear to* be arguments.
      *
      * @return  array
      */
-    public function arguments() : array;
+    public function getArguments() : array;
 
     /**
-     * Returns all tokens which start with a hyphen and therefore *appear to* be options, without
-     * differentiating between short and long options.
+     * Returns all tokens which *appear to* be options, without differentiating between short and long options.
      *
      * @return  array
      */
-    public function options() : array;
+    public function getOptions() : array;
 }
