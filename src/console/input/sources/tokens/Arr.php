b2ebb121d4979d0b4fdcbe5871d3fbd4f689df79
Michał Chojnacki <m.chojnacki@muyo.io>
2017-02-06 13:01:52 +0100

    [Console/Input] Added input\sources\tokens\Arr to store raw input in form of parameter -> value map contained in an array.
    The collection is able to resolve both Input Arguments and Options by name.

---
+++ src/console/input/sources/tokens/Arr.php
@@ -0,0 +1,52 @@
+<?php namespace nyx\console\input\sources\tokens;
+
+// External dependencies
+use nyx\core\collections;
+
+// Internal dependencies
+use nyx\console\interfaces;
+
+/**
+ * Array Input Tokens
+ *
+ * This Tokens collection is able to resolve both Input Arguments and Options by name.
+ *
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
+ */
+class Arr extends collections\Map implements interfaces\input\Tokens
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getArguments() : array
+    {
+        $arguments = [];
+
+        foreach ($this->items as $name => $value) {
+            if ($name[0] !== '-') {
+                $arguments[$name] = $value;
+            }
+        }
+
+        return $arguments;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOptions() : array
+    {
+        $options = [];
+
+        foreach ($this->items as $name => $value) {
+            if ($name[0] === '-' && $name !== '--') {
+                $options[$name] = $value;
+            }
+        }
+
+        return $options;
+    }
+}
