bb88d4cda3697aa62d1b23e4e395d2b315020903
Michał Chojnacki <m.chojnacki@muyo.io>
2017-02-06 12:57:04 +0100

    [Console/Input] Added input\sources\tokens\Argv to store raw input in the argv format.
    The collection is able to resolve Input Options by name, as the name would be contained in the argv data, but will not be able to do the same for Input Arguments, as their names are unknown until the input gets bound to a Definition.

---
+++ src/console/input/sources/tokens/Argv.php
@@ -0,0 +1,95 @@
+<?php namespace nyx\console\input\sources\tokens;
+
+// External dependencies
+use nyx\core;
+
+// Internal dependencies
+use nyx\console\interfaces;
+
+/**
+ * Argv Input Tokens
+ *
+ * This Tokens collection is able to resolve Input Options by name, as the name is contained in the argv data,
+ * but will not be able to do the same for Input Arguments, as their names are unknown until the input gets
+ * bound to a Definition.
+ *
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
+ */
+class Argv extends core\collections\Map implements interfaces\input\Tokens
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function get($parameter, $default = null)
+    {
+        foreach ($this->items as $token) {
+
+            if ($token === $parameter || 0 === strpos($token, $parameter . '=')) {
+                if (false !== $pos = strpos($parameter, '=')) {
+                    return substr($token, $pos + 1);
+                }
+
+                return $token;
+            }
+        }
+
+        return $default;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has($parameter) : bool
+    {
+        return in_array($parameter, $this->items);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove($parameter) : core\collections\interfaces\Map
+    {
+        if (false !== $key = array_search($parameter, $this->items)) {
+            unset($this->items[$key]);
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getArguments() : array
+    {
+        $arguments = [];
+
+        foreach ($this->items as $token) {
+            if ($token[0] !== '-') {
+                $arguments[] = $token;
+            }
+        }
+
+        return $arguments;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOptions() : array
+    {
+        $options = [];
+
+        foreach ($this->items as $token) {
+            if ($token[0] === '-' && $token !== '--') {
+                $options[] = $token;
+            }
+        }
+
+        return $options;
+    }
+}
