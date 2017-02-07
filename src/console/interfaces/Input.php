49c327722e709f27dc22be664e1c7f6116c9e448
Michał Chojnacki <m.chojnacki@muyo.io>
2017-02-07 13:16:51 +0100

    [Console/Input] Explained a few caveats about the lifecycle of Parameter Values collections in the description of interfaces\Input.

--- src/console/interfaces/Input.php
+++ src/console/interfaces/Input.php
@@ -9,6 +9,13 @@ use nyx\console;
  * Wraps access to Input Parameters (Arguments and Options). However, only the raw (unmapped and not validated)
  * input tokens are available until the instance gets bound to a Definition.
  *
+ * Whenever the instance gets bound to a Definition, the Arguments and Options collections get rebuilt from
+ * scratch as new instances, meaning pointers to previous collections effectively are likely to become invalid
+ * for the current input.
+ *
+ * The Input instance itself will generally persist throughout an execution cycle and therefore it is recommended
+ * to access the Parameters via the Input instance instead of maintaining direct pointers to them.
+ *
  * @version     0.1.0
  * @author      Michal Chojnacki <m.chojnacki@muyo.io>
  * @copyright   2012-2017 Nyx Dev Team
