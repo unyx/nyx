f303ca5bb501076dcd388410b29997dfc6a31cd1
Michał Chojnacki <m.chojnacki@muyo.io>
2017-02-08 18:01:04 +0100

    [Core/Collections] Added exceptions\KeyNotExists.

---
+++ src/core/collections/exceptions/KeyNotExists.php
@@ -0,0 +1,42 @@
+<?php namespace nyx\core\collections\exceptions;
+
+// Internal dependencies
+use nyx\core\collections;
+
+/**
+ * Key Does Not Exist In Collection Exception
+ *
+ * @version     0.1.0
+ * @author      Michal Chojnacki <m.chojnacki@muyo.io>
+ * @copyright   2012-2017 Nyx Dev Team
+ * @link        https://github.com/unyx/nyx
+ */
+class KeyNotExists extends collections\Exception
+{
+    /**
+     * @var mixed   The key that was not found.
+     */
+    private $key;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param   mixed   $key    The key that was not found.
+     */
+    public function __construct(collections\interfaces\Collection $collection, $key, string $message = null, $code = 0, $previous = null)
+    {
+        $this->key = $key;
+
+        parent::__construct($collection, $message ?? "No item with the key [$key] exists in the Collection.", $code, $previous);
+    }
+
+    /**
+     * Returns the key that was not found.
+     *
+     * @return  mixed
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+}
